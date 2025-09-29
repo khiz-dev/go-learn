23106d308f56c84fe5266e4a7c4b4fd636470ab0
Khizar Malik <36198335+khiz-dev@users.noreply.github.com>
2025-09-29 23:58:51 +0100

    Chapter2: Multi assignment go

---
+++ chapter-2/assign/main.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+	var (
+		maxSpeed         int     = 600
+		maxSpinSpeed     int     = 400
+		maxDarkThreshold int     = 500
+		isActive         bool    = true
+		isCalibrated     bool    = false
+		maxRatio         float32 = 1.34e22
+	)
+
+	println("Max Speed:", maxSpeed)
+	println("Max Spin Speed:", maxSpinSpeed)
+	println("Max Dark Threshold:", maxDarkThreshold)
+	println("Is Active:", isActive)
+	println("Is Calibrated:", isCalibrated)
+	println("Max Ratio:", maxRatio)
+}
